93d21d37ede89a500088ed3efca709419076df51
Chetan Salotra <chetan.salotra4git@gmail.com>
2025-10-09 19:00:43 -0500

    Initial commit with project setup

---
+++ BulkyBookWeb/Program.cs
@@ -0,0 +1,37 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+var isDev = builder.Environment.IsDevelopment();
+
+// Add services to the container.
+var mvcBuilder = builder.Services.AddControllersWithViews();
+
+if (isDev)
+{
+    mvcBuilder.AddRazorRuntimeCompilation();
+}
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
