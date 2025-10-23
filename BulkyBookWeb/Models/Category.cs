c62deaeea47960d9565e78ac529506df4b2a5ba0
Chetan Salotra <chetan.salotra4git@gmail.com>
2025-10-22 22:18:15 -0500

    Added functionality to create edit and delete categories

--- BulkyBookWeb/Models/Category.cs
+++ BulkyBookWeb/Models/Category.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace BulkyBookWeb.Models
 {
@@ -7,7 +8,9 @@ namespace BulkyBookWeb.Models
         [Key] //data annotation attribute allows you to use data annotations
         public int Id { get; set; }
         [Required]
-        public string Name { get; set; }  
+        public string Name { get; set; }
+        [DisplayName("Display Order")]
+        [Range(1,1000, ErrorMessage ="Display order must be between 1 and 1000!!")]
         public string DisplayOrder { get; set; }
         public DateTime CreatedDateTime { get; set; } = DateTime.Now;
     }
